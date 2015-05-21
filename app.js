e193d34864429e5930937fba116c586388e4af4e
ok2ju <soulSpringg@gmail.com>
2015-05-21 19:37:03 +0300

    repo init

---
+++ app.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var config = require('./config');
+var mongoose = require('./lib/mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var HttpError = require('./error').HttpError;
+
+var routes = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(require('./middleware/sendHttpError'));
+
+app.use(session({
+  secret: config.get('session:secret'),
+  key: config.get('session:key'),
+  cookie: config.get('session:cookie'),
+  store: new MongoStore({mongooseConnection: mongoose.connection})
+}));
+
+require('./routes')(app);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(err, req, res, next) {
+  if(typeof err == 'number') {
+    err = new HttpError(err);
+  }
+
+  if(err instanceof HttpError) {
+    res.sendHttpError(err);
+  } else {
+    if(app.get('env') == 'development') {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: err
+      });
+    } else {
+      log.error(err);
+      err = new HttpError(500);
+      res.sendHttpError(err);
+    }
+  }
+});
+
+module.exports = app;
